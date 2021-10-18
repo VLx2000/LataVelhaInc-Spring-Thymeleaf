bc04a64e46f1723c8891165be4e76273e409d67e
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-18 13:43:20 -0300

    adicionando maven e requisitos

---
+++ LataVelhaInc/src/main/java/br/ufscar/dc/dsw/LataVelhaInc/LataVelhaIncApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.LataVelhaInc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LataVelhaIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LataVelhaIncApplication.class, args);
+	}
+
+}
