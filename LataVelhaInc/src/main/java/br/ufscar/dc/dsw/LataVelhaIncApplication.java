1ffb1d434229b54e2b0cf56b963b5c1687d8751d
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-18 14:54:37 -0300

    adicionando pagina inicial LataVelhaInc.

--- LataVelhaInc/src/main/java/br/ufscar/dc/dsw/LataVelhaIncApplication.java
+++ LataVelhaInc/src/main/java/br/ufscar/dc/dsw/LataVelhaIncApplication.java
@@ -1,4 +1,4 @@
-package br.ufscar.dc.dsw.LataVelhaInc;
+package br.ufscar.dc.dsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
