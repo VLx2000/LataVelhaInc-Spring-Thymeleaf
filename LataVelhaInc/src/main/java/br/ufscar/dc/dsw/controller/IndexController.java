d25b9ca9de6691a4234ec904e01bccb56205f1fb
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-19 22:04:33 -0300

    adicionando carros na tela inicial

--- LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/IndexController.java
+++ LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/IndexController.java
@@ -1,12 +1,21 @@
 package br.ufscar.dc.dsw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.ufscar.dc.dsw.service.spec.IVeiculoService;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+	private IVeiculoService service;
+
     @GetMapping("/")
-    public String index() {
+    public String index(ModelMap model) {
+        model.addAttribute("catalogo", service.buscarTodos());
         return "index";
     }
 }
