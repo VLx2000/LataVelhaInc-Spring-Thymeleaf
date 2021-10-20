f6c52ee46b6b342ab87b7c1e54bfbf0c216437cc
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-20 18:50:34 -0300

    adicionando remoçao e ediçao de cliente e remoçao de loja

--- LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/AdminController.java
+++ LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/AdminController.java
@@ -1,14 +1,21 @@
 package br.ufscar.dc.dsw.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-//import br.ufscar.dc.dsw.domain.Cliente;
+import br.ufscar.dc.dsw.domain.Cliente;
 import br.ufscar.dc.dsw.service.spec.IClienteService;
-//import br.ufscar.dc.dsw.domain.Loja;
+import br.ufscar.dc.dsw.domain.Loja;
 import br.ufscar.dc.dsw.service.spec.ILojaService;
 
 @Controller
@@ -25,15 +32,61 @@ public class AdminController {
 		return "admin/crudLinks";
 	}
 
+    @GetMapping("/listarClientes")
+	public String listarClientes(ModelMap model) {
+        model.addAttribute("listaClientes", serviceCliente.buscarTodos());
+		return "admin/listaClientes";
+	}
+
+	@GetMapping("/cadastrarCliente")
+	public String cadastroCliente(ModelMap model) {
+		return "admin/cadastroCliente";
+	}
+
+	@GetMapping("/editarCliente*")
+	public String preEdicaoCliente(@RequestParam Long id, ModelMap model) {
+        model.addAttribute("cliente", serviceCliente.buscarPorId(id));
+		return "admin/cadastroCliente";
+	}
+
+	@PostMapping("/editarCliente")
+	public String EdicaoCliente(@Valid Cliente cliente, ModelMap model, BindingResult result, RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "admin/cadastroCliente";
+		}
+
+		serviceCliente.salvar(cliente);
+		attr.addFlashAttribute("sucess", "cliente.edit.sucess");
+		return "redirect:/admin/listarClientes";
+	}
+
+	@GetMapping("/removerCliente")
+	public String remocaoCliente(ModelMap model, @RequestParam Long id) {
+        serviceCliente.excluir(id);
+		return "redirect:/admin/listarClientes";
+	}
+
+	@PostMapping("/salvarCliente")
+	public String salvar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attr) {
+
+		if (result.hasErrors()) {
+			return "admin/cadastroCliente";
+		}
+		serviceCliente.salvar(cliente);
+		attr.addFlashAttribute("sucess", "cliente.create.sucess");
+		return "redirect:/admin/listarClientes";
+	}
+
 	@GetMapping("/listarLojas")
 	public String listarLojas(ModelMap model) {
 		model.addAttribute("listaLojas", serviceLoja.buscarTodos());
 		return "admin/listaLojas";
 	}
 
-    @GetMapping("/listarClientes")
-	public String listarClientes(ModelMap model) {
-        model.addAttribute("listaClientes", serviceCliente.buscarTodos());
-		return "admin/listaClientes";
+	@GetMapping("/removerLoja")
+	public String remocaoLoja(ModelMap model, @RequestParam Long id) {
+        serviceLoja.excluir(id);
+		return "redirect:/admin/listarLojas";
 	}
 }
