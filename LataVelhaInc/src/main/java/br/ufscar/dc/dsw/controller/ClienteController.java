8dbbc99bd5252bc0449e68f9096dfce2d51d999c
Rafael Murata <ryy.murata@gmail.com>
2021-11-03 16:57:38 -0300

    Internacionalizando placeholders dos campos de de cadastro de cliente e loja

--- LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/ClienteController.java
+++ LataVelhaInc/src/main/java/br/ufscar/dc/dsw/controller/ClienteController.java
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ufscar.dc.dsw.domain.Cliente;
-import br.ufscar.dc.dsw.domain.Proposta;
+
 import br.ufscar.dc.dsw.service.spec.IClienteService;
 import br.ufscar.dc.dsw.service.spec.IPropostaService;
 
-import java.util.List;
 import javax.validation.Valid;
 
 @Controller
